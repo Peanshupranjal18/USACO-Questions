6ceb9a20cd8ea9992553b68728481587c350b489
Pranshu Pranjal <pprnch@gmail.com>
2023-11-04 16:59:29 +0530

    Create timeismoney.cpp
    We only need to keep track of the number of days that Bessie has been traveling and the amount of mooney that she has made. For every day up to
    $T_{\text{max}}$
    , we'll try to travel to each city, and update the mooney made if the current path is more optimal. The maximum amount of money she makes is
    $1000t - t^2$
    , in the case where she makes
    $1000$
     mooney per city and the cost is
    $1$
    . Her earnings become negative when
    $t > 1000$
    , so we only have to check her movement across the cities for at most
    $1000$
     days

---
+++ timeismoney.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define intt long long int
+#define ll long long
+#define ld long double
+#define MOD 1000000007
+#define f(i, n) for (intt i = 0; i < n; i++)
+#define ff(i, a, b) for (intt i = a; i < b; i++)
+#define pb push_back
+#define ii pair<intt, intt>
+#define vi vector<intt>
+#define vvi vector<vector<intt>>
+#define vpi vector<pair<intt, intt>>
+#define fi first
+#define sec second
+#define all(x) x.begin(), x.end()
+#define allr(x) x.rbegin(), x.rend()
+#define minv *min_element
+#define maxv *max_element
+#define rt return
+#define um unordered_map
+#define acc accumulate
+#define sz(x) x.size()
+#define ub upper_bound
+#define lb lower_bound
+#define mt multiset
+#define rs resize
+#define mah priority_queue<intt>
+#define mih priority_queue<intt, vector<intt>, greater<intt>>
+#define mahp priority_queue<pair<intt, intt>>
+#define mihp priority_queue<pair<intt, intt>, vector<pair<intt, intt>>, greater<pair<intt, intt>>>
+using namespace std;
+using namespace __gnu_pbds;
+
+// Right Left Up Down
+intt dx[] = {0, 0, 1, -1};
+intt dy[] = {1, -1, 0, 0};
+intt n, m, a, b, c;
+
+bool possible(int x, int y)
+{
+    return (x < n && x >= 0 && y < m && y >= 0);
+}
+
+vi v, v1, v2, v3, v4;
+vi adj[200001];
+
+bool isPrime(intt n)
+{
+    if (n <= 1)
+        return false;
+    if (n == 2 || n == 3)
+        return true;
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+    for (intt i = 5; i <= sqrt(n); i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
+
+// oset<int>s:s.find_by_order(k):Kth element in s,s.order_of_key(k):Number of item strictly lessthan k
+template <class T>
+using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const intt MAX_DAYS = 1000;
+
+void solve()
+{
+    cin >> n >> m >> a;
+    v.rs(n);
+    f(i, n) cin >> v[i];
+    f(i, m)
+    {
+        cin >> b >> c;
+        adj[--b].pb(--c);
+    }
+    vvi dp(MAX_DAYS + 1, vi(n, -1));
+    dp[0][0] = 0;
+    intt ans = 0;
+    f(d, MAX_DAYS)
+    {
+        f(i, n)
+        {
+            if (dp[d][i] != -1)
+            {
+                for (intt u : adj[i])
+                {
+                    dp[d + 1][u] = max(dp[d + 1][u], dp[d][i] + v[u]);
+                }
+            }
+        }
+        ans = max(ans, (dp[d][0] - (a * d * d)));
+    }
+    cout << ans << "\n";
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // intt tc;cin>>tc;while(tc--)
+    solve();
+    return 0;
+}
